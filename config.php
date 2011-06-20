589f5632a48234f5102ec7b8cfbe7166f888e8b9
Comus Leong <good2000mo@hotmail.com>
2011-06-20 09:35:08 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_type = 'mysqli';
+$db_host = 'localhost';
+$db_name = 'jose';
+$db_username = 'jose';
+$db_password = '23230647';
+$db_prefix = 'jose_';
+$p_connect = false;
+
+$cookie_name = 'pun_cookie_7af630';
+$cookie_domain = '';
+$cookie_path = '/';
+$cookie_secure = 0;
+$cookie_seed = '2e1122ffc3e16f6f';
+
+define('PUN', 1);
